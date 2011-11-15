02a5a81fa918ec9c293d30f51bbb95e7103fe70e
Andy Gordon <andy@carbon-hub.com>
2011-11-15 12:22:01 +0000

    init

---
+++ app/assets/javascripts/general.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#loaded").html("Javascript loaded correctly from '/externals/application.js'")
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aptest::Application.config.secret_token = '107ce7cb8f69c03e0bdeb4c52e965027c5152724b49dfb7739fc8ed1fd2d417946f7aed05becda5763b52a1c128d3e610bb22079ca3e8b0deda97eaaec7f08f0'
